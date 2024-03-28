a219e8dd94026f1f5af372ffce82358aa1e1b45a
DKVI <92564346+DKVI@users.noreply.github.com>
2024-03-28 17:21:23 +0700

    fix 404 page not found

--- views/e404.php
+++ views/e404.php
@@ -1,4 +1,4 @@
-<link href="../css/admin.css" rel="stylesheet">
+<link href="./css/admin.css" rel="stylesheet">
 <div class="container">
     <div class="m-auto">
         <h1>404 - Page Not Found</h1>
@@ -10,28 +10,28 @@
 </div>
 
 <style>
-body {
-    font-family: Arial, sans-serif;
-    background-color: #f5f5f5;
-    text-align: center;
-}
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #f5f5f5;
+        text-align: center;
+    }
 
-.container {
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    z-index: 300;
-}
+    .container {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        z-index: 300;
+    }
 
-h1 {
-    color: #333333;
-    font-size: 36px;
-    margin-bottom: 30px;
-}
+    h1 {
+        color: #333333;
+        font-size: 36px;
+        margin-bottom: 30px;
+    }
 
-p {
-    color: #666666;
-    font-size: 18px;
-    margin-bottom: 30px;
-}
+    p {
+        color: #666666;
+        font-size: 18px;
+        margin-bottom: 30px;
+    }
 </style>
