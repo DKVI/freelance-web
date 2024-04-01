6685e8afe22015dd57cf018cc3b611f55d96b57f
DKVI <92564346+DKVI@users.noreply.github.com>
2024-04-01 21:26:28 +0700

    add admin controller

--- views/admin/components/adminHeader.php
+++ views/admin/components/adminHeader.php
@@ -1,19 +1,25 @@
-<nav class="p-3 admin-header d-flex navbar navbar-expand-lg justify-content-between "
-    style="background-color: #274069; height: 60px;">
+<nav class="p-3 admin-header d-flex navbar navbar-expand-lg justify-content-between " style="background-color: #274069; height: 60px;">
     <div class="px-5 w-1/3 h-100" style="width: 30%;">
         <a style="cursor: pointer;" href="<?php echo BASE_URL . '/home' ?>">
             <img class=" h-100" src="../assets/imgs/logo-gray.png" style="scale: 2;" alt="">
         </a>
     </div>
     <div class="d-flex justify-content-between" style="width: 30%;">
-        <a class="text-white text-decoration-none" href="<?php echo BASE_URL . '/admin/home' ?>"
-            style="cursor: pointer">Home</a>
+        <a class="text-white text-decoration-none" href="<?php echo BASE_URL . '/admin/home' ?>" style="cursor: pointer">Home</a>
         <a class="text-white text-decoration-none" href="<?php echo BASE_URL . '/admin/posts' ?>" style=" cursor:
             pointer">Posts</a>
-        <a class="text-white text-decoration-none" href="<?php echo BASE_URL . '/admin/messages' ?>"
-            style="cursor: pointer">Messages</a>
+        <a class="text-white text-decoration-none" href="<?php echo BASE_URL . '/admin/messages' ?>" style="cursor: pointer">Messages</a>
     </div>
     <div class="px-5 w-1/3 d-flex justify-content-end " style="width: 30%;">
-        <a class="btn btn-outline-light" href="../controllers/handleLogout.php">Logout</a>
+        <div class="px-4 dropdown show">
+            <a class="btn btn-outline-light dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Admin
+            </a>
+
+            <div class="dropdown-menu shadow " aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="<?php echo BASE_URL . "/admin/verifyEmail" ?>">Change Password</a>
+                <a class="dropdown-item" href="<?php echo BASE_URL . "/admin/login" ?>">Logout</a>
+            </div>
+        </div>
     </div>
 </nav>
--- views/admin/verifyEmail.php
+++ views/admin/verifyEmail.php
@@ -1,9 +1,7 @@
-<?php
-session_destroy();
-?>
 <link rel="stylesheet" href="../css/admin.css">
 <div class="min-vh-100 min-vw-100 d-flex">
-    <form method="post" action="../controllers/handleSendMail.php" class="form d-flex w-50 m-auto rounded-3 shadow overflow-hidden" style="height: 80%">
+    <form method="post" action="../controllers/handleSendMail.php"
+        class="form d-flex w-50 m-auto rounded-3 shadow overflow-hidden" style="height: 80%">
         <div class="shadow w-50 d-flex p-5" style="background-color: #274069;">
             <img src="../assets/imgs/logo-gray.png" alt="" class="m-auto w-100">
         </div>
@@ -12,7 +10,8 @@ session_destroy();
                 <h1 class="py-4 text-center" style="color: #274069;">VERIFY ADMIN</h1>
                 <div class="form-group">
                     <div class="pb-2">
-                        <input id="email" type="email" name="email" class="form-control shadow-sm  py-2 px-3" placeholder="Enter admin's email" required>
+                        <input id="email" type="email" name="email" class="form-control shadow-sm  py-2 px-3"
+                            placeholder="Enter admin's email" required>
                     </div>
                 </div>
                 <button class="btn mt-4" style="background-color: #274069; color: white">Confirm</button>
