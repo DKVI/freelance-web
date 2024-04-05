8fa39e74ee4a7ec5fd2859da8e2c6d33a477af1a
DKVI <92564346+DKVI@users.noreply.github.com>
2024-04-05 12:29:17 +0700

    fix index.php

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css" />
-    <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
-
-</head>
-<body class="custom-scrollbar">
-    <?php
+<?php
     session_start();
     include_once './config.php';
     include_once "./vendor/bootstrap.php";
@@ -27,36 +14,38 @@
 
     ?>
 
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="icon" type="image/x-icon" href="<?php echo BASE_URL ?>/assets/imgs/favicon.ico">
-        <title>Mooting Summer School</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css" />
-        <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" type="image/x-icon" href="<?php echo BASE_URL ?>/assets/imgs/favicon.ico">
+    <title>Mooting Summer School</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css" />
+    <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+</head>
 
-    <?php
+<?php
     $language = "";
     if (isset($_GET['language'])) {
         $language = $_GET['language'];
     }
     ?>
 
-    <body class="custom-scrollbar">
+<body class="custom-scrollbar">
 
-        <link href="css/base.css">
-        <!-- Add layout using router -->
-        <div class="header">
-            <!-- Add header to all pages -->
-            <?php include __DIR__ . "/views/components/header.php" ?>
-        </div>
-        <div class="main">
-            <?php
+    <link href="css/base.css">
+    <!-- Add layout using router -->
+    <div class="header">
+        <!-- Add header to all pages -->
+        <?php include __DIR__ . "/views/components/header.php" ?>
+    </div>
+    <div class="main">
+        <?php
             $route = $segments[2] . (isset($segments[3]) ?  '/' . $segments[3] : "");
             switch ($route) {
                 case "":
@@ -103,34 +92,35 @@
                     break;
             }
             ?>
-            <!-- Add footer to all pages -->
-            <div class="footer">
-                <?php include __DIR__ . "/views/components/footer.php" ?>
-            </div>
+        <!-- Add footer to all pages -->
+        <div class="footer">
+            <?php include __DIR__ . "/views/components/footer.php" ?>
         </div>
-        <script>
-            const vnElements = document.querySelectorAll(".vn");
-            const engElements = document.querySelectorAll(".eng");
-            const queryParams = "<?php echo $language ?>";
-            if (queryParams === "" || queryParams === "vn") {
-                vnElements.forEach((e) => {
-                    e.classList.remove("d-none");
-                });
-                engElements.forEach((e) => {
-                    e.classList.add("d-none");
-                });
-            } else {
-                vnElements.forEach((e) => {
-                    e.classList.add("d-none");
-                });
-                engElements.forEach((e) => {
-                    e.classList.remove("d-none");
-                });
-            }
-        </script>
-        <script src="js/index.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-        </script>
-    </body>
+    </div>
+    <script>
+    const vnElements = document.querySelectorAll(".vn");
+    const engElements = document.querySelectorAll(".eng");
+    const queryParams = "<?php echo $language ?>";
+    if (queryParams === "" || queryParams === "vn") {
+        vnElements.forEach((e) => {
+            e.classList.remove("d-none");
+        });
+        engElements.forEach((e) => {
+            e.classList.add("d-none");
+        });
+    } else {
+        vnElements.forEach((e) => {
+            e.classList.add("d-none");
+        });
+        engElements.forEach((e) => {
+            e.classList.remove("d-none");
+        });
+    }
+    </script>
+    <script src="js/index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
 
-    </html>
+</html>
