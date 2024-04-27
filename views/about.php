1ed99b73a443b3f7f1a2453f46a11a8e9c2648b3
Thi Lam <z.thiilam@gmail.com>
2024-04-27 23:08:16 +0700

    Home page eng ver

--- views/about.php
+++ views/about.php
@@ -1,17 +1,25 @@
 <main id="ab-main">
-    <section class="container-fluid">
+    <section class="container">
         <div class="d-lg-flex">
-            <img src="./assets/imgs/about-1.png" alt="founder-Vo-Son-Hoang">
-            <div>
-                <p>Chào mừng các bạn đến với Mooting Summer School (MSS).</p>
-                <p>Mình tin rằng mooting là một hoạt động mà mọi sinh viên luật đều nên trải nghiệm một lần trong cuộc đời học luật. Dưới góc độ chuyên môn, đây là sân chơi đầy thử thách không chỉ giúp các bạn rèn luyện tư duy phản biện, nâng cao kỹ năng nghiên cứu, mà còn mở mang sự hiểu biết về pháp luật của mình. Ngoài ra, về khía cạnh quan hệ, mooting cũng tạo cơ hội để bạn gặp gỡ, giao lưu với những người giỏi trong ngành, những người có cùng đam mê, sở thích, hay thậm chí những người “đồng nghiệp tương lai” của các bạn sau này.</p>
-                <p>Trong bất kỳ chuyện chinh phục nào, sự yêu và hiểu đối phương luôn là điều tối quan trọng. Vậy nên, tại MSS, mình, với tư cách là Nhà sáng lập và Người hướng dẫn, hy vọng có thể khiến bạn “yêu” và “hiểu” mooting hơn qua những bài giảng, những lời khuyên và những câu chuyện được chia sẻ. Và rồi, kể cả khi chinh phục được cuộc thi moot nào đó hay không, các bạn đều có thể trở thành mình – người truyền cảm hứng, dẫn dắt cho các thế hệ mooter tiếp theo. Từ đó, tất cả chúng ta sẽ góp phần nâng cao chất lượng và trình độ chung của cộng đồng mooting tại Việt Nam.</p>
-                <p>Cảm ơn các bạn đã cho mình và MSS cơ hội được đồng hành cùng với các bạn trong chặng đường mooting. Mình hy vọng sẽ được gặp các bạn tại MSS.</p>
+            <img src="./assets/imgs/about-1.png" alt="founder-Vo-Son-Hoang" style="height: 100%;">
+            <div class="px-5 position-relative" style="font-size: 1.1rem;">
+                <div class="founder-words">
+                    <p>Chào mừng các bạn đến với Mooting Summer School (MSS).</p>
+                    <p>Mình tin rằng mooting là một hoạt động mà mọi sinh viên luật đều nên trải nghiệm một lần trong cuộc đời học luật. Dưới góc độ chuyên môn, đây là sân chơi đầy thử thách không chỉ giúp các bạn rèn luyện tư duy phản biện, nâng cao kỹ năng nghiên cứu, mà còn mở mang sự hiểu biết về pháp luật của mình. Ngoài ra, về khía cạnh quan hệ, mooting cũng tạo cơ hội để bạn gặp gỡ, giao lưu với những người giỏi trong ngành, những người có cùng đam mê, sở thích, hay thậm chí những người “đồng nghiệp tương lai” của các bạn sau này.</p>
+                    <p>Trong bất kỳ chuyện chinh phục nào, sự yêu và hiểu đối phương luôn là điều tối quan trọng. Vậy nên, tại MSS, mình, với tư cách là Nhà sáng lập và Người hướng dẫn, hy vọng có thể khiến bạn “yêu” và “hiểu” mooting hơn qua những bài giảng, những lời khuyên và những câu chuyện được chia sẻ. Và rồi, kể cả khi chinh phục được cuộc thi moot nào đó hay không, các bạn đều có thể trở thành mình – người truyền cảm hứng, dẫn dắt cho các thế hệ mooter tiếp theo. Từ đó, tất cả chúng ta sẽ góp phần nâng cao chất lượng và trình độ chung của cộng đồng mooting tại Việt Nam.</p>
+                    <p>Cảm ơn các bạn đã cho mình và MSS cơ hội được đồng hành cùng với các bạn trong chặng đường mooting. Mình hy vọng sẽ được gặp các bạn tại MSS.</p>
+                </div>
+                <h5 class="text-end mt-5">Đọc thêm thông tin về Nhà sáng lập tại đây</h5>
+                <div class="founder-words-img position-absolute">
+                    <h6 style="font-size:4.2rem; line-height:0.5rem; ">Lời ngỏ từ</h6>
+                    <h6 style="font-size:10.5rem; margin:0; padding:0;">Nhà sáng lập</h6>
+                </div>
+                <!-- <img src="./assets/imgs/about-2.png" alt="Words-from-our-Founder" class="founder-words-img position-absolute" style="left: 2rem; bottom: -3rem;"> -->
             </div>
         </div>
     </section>
 
-    <section class="" id="services">
+    <!-- <section class="" id="services">
         <div class="container">
             <div class="text-center">
                 <h1 class="section-heading text-uppercase">Our Team</h1>
@@ -21,7 +29,6 @@
                     <span class="fa-stack fa-4x">
                         <a href="#">
                             <i class="fas fa-circle fa-stack-2x main-text-color"></i>
-                            <!-- <i class="fas fa-solid fa-eye fa-stack-1x fa-inverse"></i> -->
                         </a>
                     </span>
                     <h4 class="my-3">Name</h4>
@@ -31,7 +38,7 @@
                     <span class="fa-stack fa-4x">
                         <a href="#">
                             <i class="fas fa-circle fa-stack-2x main-text-color"></i>
-                            <!-- <i class="fas fa-solid fa-mortar-pestle fa-stack-1x fa-inverse"></i> -->
+                           
                         </a>
                     </span>
                     <h4 class="my-3">Name</h4>
@@ -42,7 +49,7 @@
                     <span class="fa-stack fa-4x">
                         <a href="#">
                             <i class="fas fa-circle fa-stack-2x main-text-color"></i>
-                            <!-- <i class="fas far fa-grin-hearts fa-stack-1x fa-inverse"></i> -->
+                            
                         </a>
                     </span>
                     <h4 class="my-3">Name</h4>
@@ -51,5 +58,5 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
 </main>
--- views/home.php
+++ views/home.php
@@ -63,9 +63,37 @@
 
         </div>
     </div>
+    <div class=" container py-5 p-lg-5 eng">
+        <h2 class="text-uppercase fw-bold display-5">WELCOME TO</h2>
+        <h1 class="text-uppercase fw-bold  display-3">Mooting Summer School</h1>
+        <div class="col-lg-8 m">
+            <p>Someone once said that when participating in mooting, we feel like we're diving into space. we won't know how far we can go or what kind of value we can gain from the experience. But first and foremost, just from the very first steps, we can be sure that this journey will not be an easy one. </p>
+            <p>
+                Therefore, if we see each moot competition as a different star in the universe,<strong style="font-family: 'Times New Roman', Times, serif; font-size: 1.3rem"><em> Non est ad astra mollis e terris
+                via</em></strong> (translated: <em>There is no easy from the earth to the stars</em>) is the philosophy of Mooting Summer School. For us, no one but you will have to choose your own "lucky star" and find the suitable path for yourself. What Mooting Summer School can help you with is to build a foundational stepping stone, a launching pad, which will make every journey with mooting less challenging. 
+            </p>
+            <p>So, shall we give each other the opportunity to get closer to our expectations?</p>
+        </div>
+        <div class="row mt-5">
+            <div class="col-lg-6">
+                <h1 class="text-uppercase fw-bold display-3 ">PROJECT</h1>
+                <p>Mooting Summer School (“<strong> MSS</strong>”) is the first professional mooting training project in Vietnam, founded and led by a former mooter who has been an active member of the community for over half a decade, serving as a mooter, coach, speaker, and judge, both domestically and internationally. </p>
+                <p>MSS will be held annually every summer and operates on a not-for-profit basis.</p>
+                <p>Non est ad astra mollis e terris via, is MSS a place to open up a less difficult path to mooting?"</p>
+                <h4><a href="<?php echo BASE_URL ?>/about" ?>See also >></a></h4>
+                <h4>REGISTRATION FOR MSS CLASS OF 2024 IS NOW OPENED</h4>
+            </div>
+            <div class="col-lg-6 mt-4">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/-pHfPJGatgE?si=KlPHj-aaR5W99vtc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="w-100"></iframe>
+            </div>
+        </div>
+    </div>
 </main>
 
 
 
 
-<!-- <div class="vn">xin chao</div> -->
+<!-- <div class="w-100">
+    <div class="eng"><?php include "./views/components/book.php" ?></div>
+
+</div> -->
