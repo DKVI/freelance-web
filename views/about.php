cb7665a422f47734ffc70c7b20fff4740cbf444e
Thi Lam <z.thiilam@gmail.com>
2024-04-28 07:47:55 +0700

    update about page

--- views/about.php
+++ views/about.php
@@ -3,19 +3,64 @@
         <div class="d-lg-flex">
             <img src="./assets/imgs/about-1.png" alt="founder-Vo-Son-Hoang" style="height: 100%;">
             <div class="px-5 position-relative" style="font-size: 1.1rem;">
-                <div class="founder-words">
+                <div class="founder-words vn">
                     <p>Chào mừng các bạn đến với Mooting Summer School (MSS).</p>
                     <p>Mình tin rằng mooting là một hoạt động mà mọi sinh viên luật đều nên trải nghiệm một lần trong cuộc đời học luật. Dưới góc độ chuyên môn, đây là sân chơi đầy thử thách không chỉ giúp các bạn rèn luyện tư duy phản biện, nâng cao kỹ năng nghiên cứu, mà còn mở mang sự hiểu biết về pháp luật của mình. Ngoài ra, về khía cạnh quan hệ, mooting cũng tạo cơ hội để bạn gặp gỡ, giao lưu với những người giỏi trong ngành, những người có cùng đam mê, sở thích, hay thậm chí những người “đồng nghiệp tương lai” của các bạn sau này.</p>
                     <p>Trong bất kỳ chuyện chinh phục nào, sự yêu và hiểu đối phương luôn là điều tối quan trọng. Vậy nên, tại MSS, mình, với tư cách là Nhà sáng lập và Người hướng dẫn, hy vọng có thể khiến bạn “yêu” và “hiểu” mooting hơn qua những bài giảng, những lời khuyên và những câu chuyện được chia sẻ. Và rồi, kể cả khi chinh phục được cuộc thi moot nào đó hay không, các bạn đều có thể trở thành mình – người truyền cảm hứng, dẫn dắt cho các thế hệ mooter tiếp theo. Từ đó, tất cả chúng ta sẽ góp phần nâng cao chất lượng và trình độ chung của cộng đồng mooting tại Việt Nam.</p>
                     <p>Cảm ơn các bạn đã cho mình và MSS cơ hội được đồng hành cùng với các bạn trong chặng đường mooting. Mình hy vọng sẽ được gặp các bạn tại MSS.</p>
                 </div>
-                <h5 class="text-end mt-5">Đọc thêm thông tin về Nhà sáng lập tại đây</h5>
-                <div class="founder-words-img position-absolute">
-                    <h6 style="font-size:4.2rem; line-height:0.5rem; ">Lời ngỏ từ</h6>
-                    <h6 style="font-size:10.5rem; margin:0; padding:0;">Nhà sáng lập</h6>
+                <div class="founder-words eng">
+                    <p>Welcome to Mooting Summer School (MSS). </p>
+                    <p>I believe that mooting is an activity every law student should experience at least once in their law school life. From a professional perspective, this is a challenging playground that not only helps you practice critical thinking, improve your research skills, but also expand your understanding of the law. Additionally, mooting is an opportunity for you to meet and interact with incredible people in the industry, people with the same passions and hobbies, or even people who might be your “future colleagues.”</p>
+                    <p>Loving and understanding your subject is always an important factor in any conquest. Therefore, at MSS, as the founder and instructor at MSS, I aim to help you “love” and “understand” mooting more through lectures, tips, and shared stories. And then, even if you conquer a moot competition or not, you can still become an inspiration and guide for the next generation of mooters. This way, we can all contribute to improving the quality and overall level of the mooting community in Vietnam.</p>
+                    <p>Thank you for allowing MSS and me to accompany you on your mooting journey. I hope to see you at MSS.</p>
                 </div>
-                <!-- <img src="./assets/imgs/about-2.png" alt="Words-from-our-Founder" class="founder-words-img position-absolute" style="left: 2rem; bottom: -3rem;"> -->
+                <h5 class="text-end mt-5 vn">Đọc thêm thông tin về Nhà sáng lập tại đây</h5>
+                <h5 class="text-end mt-5 eng">You can read more about the Founder here</h5>
+                <div class="founder-words-img position-absolute">
+                    <h6 style="font-size:4.2rem; line-height:0.5rem; " class="vn">Lời ngỏ từ</h6>
+                    <h6 style="font-size:4.2rem; line-height:0.5rem; " class="eng">Message from</h6>
+                    <h6 style="font-size:10.5rem; margin:0; padding:0;" class="vn">Nhà sáng lập</h6>
+                    <h6 style="font-size:10.5rem; margin:0; padding:0;" class="eng">Our Founder</h6>
+                </div>
             </div>
+        </div>
+    </section>
+    <section class="our-mission background-full">
+        <div class="container p-5" style="color: #e5e5e5">
+            <div class="our-mission-title text-end vn">
+                <h1 style="font-size:10.5rem; margin:0; padding:0;">Sứ mệnh</h1>
+                <h2 style="font-size:7rem; line-height:5rem;" >của MSS</h2>
+            </div>
+            <h1 style="font-size:10.5rem; margin:0; padding:0;" class="eng">Our mission</h1>
+            <div class="col-lg-8 vn" style="margin-top: -7rem">
+                <p>Trong những năm gần đây, mooting có xu hướng phát triển và phổ biến hơn tại khắp các trường đại học đào tạo về luật ở Việt Nam. Tuy nhiên, sự phát triển trong cộng đồng dường như chưa có tính tổng quát, hệ thống, và kế thừa. Các Mooter thường không được huấn một cách chuyên nghiệp và đầy đủ, do đó gặp khá nhiều khó khăn trong hành trình chinh chiến tại các giải đấu moot từ trong nước đến nước ngoài.</p>
+                <p>Trước bối cảnh đó, MSS ra đời với hy vọng có thể đặt những viên gạch nền tảng cho sự phát triển bền vững của cộng đồng. Là dự án đào tạo chuyên nghiệp về mooting đầu tiên tại Việt Nam, MSS kỳ vọng có thể đem lại cho học viên ba giá trị như sau:</p>
+                <ol style="list-style-type: lower-roman">
+                    <li>Nắm vững kỹ năng thiết yếu để đạt hiệu quả cao khi tham gia các cuộc thi moot;</li>
+                    <li>Xây dựng tư duy và thái độ phù hợp khi tham gia đấu moot; và</li>
+                    <li>Lan tỏa và phát triển tính cộng đồng trong mooting.</li>
+                </ol>
+            </div>
+            <div class="d-flex align-items-center justify-content-center eng">
+                <div class="col-lg-8">
+                    <p>In recent years, mooting has gained popularity in law schools over the last five years. However, the moot community lacks generalization, systemization, and inheritance in its development. Mooters are not typically professionally trained or led, resulting in many challenges and obstacles when competing in moot competitions, domestically and internationally.</p>
+                    <p>Given this context, MSS was established with the hope of laying the foundational bricks for the sustainable development of the community. As the first professional training project in Vietnam, MSS aims to provide its learners with three core values, as follows:.</p>
+                    <ol style="list-style-type: lower-roman">
+                        <li>To master foundational skills to achieve high efficiency when participating in mooting competitions;</li>
+                        <li>To build appropriate mindset, spirit, and attitude when participating in mooting;</li>
+                        <li>To spread and develop a sense of community in mooting.</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mascot-moto">
+        <div class="position-absolute col-lg-5">
+            <img src="./assets/imgs/about-3.png" alt="mss-bg" class="w-100">
+        </div>
+        <div>
+            <h1 style="font-size:8.5rem; margin:0; padding:0;" class="text-end vn">Biểu tượng Griffin</h1>
         </div>
     </section>
 
