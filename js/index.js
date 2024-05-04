9199b20c085cfb16f873489efb2712df5cacb9e8
Thi Lam <z.thiilam@gmail.com>
2024-05-04 13:37:28 +0700

    About Page

--- js/index.js
+++ js/index.js
@@ -1,41 +1,14 @@
-function initTextLoop() {
-  const container = document.querySelector(".js--textloop--container");
-  const content = container.querySelector(".js--textloop--content");
-  const loopText = content.innerHTML;
-  let init = false;
-
-  while (container.offsetWidth > content.offsetWidth - container.offsetWidth) {
-    if (!init) {
-      content.innerHTML += loopText;
-      animContent(content);
-      init = true;
-    } else {
-      content.innerHTML += loopText;
-    }
-  }
-  return;
+//circle text animation
+const text = document.querySelectorAll(".text");
+for (let i = 0; i < text.length; i++) {
+  text[i].innerHTML = text[i].innerText
+    .split("")
+    .map(
+      (char, x) =>
+        `<span style="transform:rotate(${x * 9.2}deg)">${char}</span>`
+    )
+    .join("");
 }
-
-function animContent(content) {
-  //Solution via js animating with the animate method
-
-  //Solution via CSS adding keyframes to the head
-
-  const textLoopAnimation =
-    `
-              @keyframes moveLeft {
-                  0% { left: 0; }
-                  100% { left: -` +
-    content.offsetWidth +
-    `px; }
-              }
-              `;
-  const styleElement = document.createElement("style");
-  styleElement.innerHTML = textLoopAnimation;
-  document.head.appendChild(styleElement);
-}
-
-initTextLoop();
 
 // FAQ PAGE ROTATE FUNCTION
 let faqLabel = document.querySelectorAll(".faq-label");
--- views/about.php
+++ views/about.php
@@ -1,17 +1,105 @@
-<main id="ab-main">
-    <section class="container-fluid">
+<main id="ab-main" class="w-100">
+    <section class="container" style="padding-top:0">
         <div class="d-lg-flex">
-            <img src="./assets/imgs/about-1.png" alt="founder-Vo-Son-Hoang">
-            <div>
-                <p>Chào mừng các bạn đến với Mooting Summer School (MSS).</p>
-                <p>Mình tin rằng mooting là một hoạt động mà mọi sinh viên luật đều nên trải nghiệm một lần trong cuộc đời học luật. Dưới góc độ chuyên môn, đây là sân chơi đầy thử thách không chỉ giúp các bạn rèn luyện tư duy phản biện, nâng cao kỹ năng nghiên cứu, mà còn mở mang sự hiểu biết về pháp luật của mình. Ngoài ra, về khía cạnh quan hệ, mooting cũng tạo cơ hội để bạn gặp gỡ, giao lưu với những người giỏi trong ngành, những người có cùng đam mê, sở thích, hay thậm chí những người “đồng nghiệp tương lai” của các bạn sau này.</p>
-                <p>Trong bất kỳ chuyện chinh phục nào, sự yêu và hiểu đối phương luôn là điều tối quan trọng. Vậy nên, tại MSS, mình, với tư cách là Nhà sáng lập và Người hướng dẫn, hy vọng có thể khiến bạn “yêu” và “hiểu” mooting hơn qua những bài giảng, những lời khuyên và những câu chuyện được chia sẻ. Và rồi, kể cả khi chinh phục được cuộc thi moot nào đó hay không, các bạn đều có thể trở thành mình – người truyền cảm hứng, dẫn dắt cho các thế hệ mooter tiếp theo. Từ đó, tất cả chúng ta sẽ góp phần nâng cao chất lượng và trình độ chung của cộng đồng mooting tại Việt Nam.</p>
-                <p>Cảm ơn các bạn đã cho mình và MSS cơ hội được đồng hành cùng với các bạn trong chặng đường mooting. Mình hy vọng sẽ được gặp các bạn tại MSS.</p>
+            <img src="./assets/imgs/about-1.png" alt="founder-Vo-Son-Hoang" style="height: 100%;">
+            <div class="px-5 position-relative" style="font-size: 1.1rem;">
+                <div class="founder-words vn">
+                    <p>Chào mừng các bạn đến với Mooting Summer School (MSS).</p>
+                    <p>Mình tin rằng mooting là một hoạt động mà mọi sinh viên luật đều nên trải nghiệm một lần trong cuộc đời học luật. Dưới góc độ chuyên môn, đây là sân chơi đầy thử thách không chỉ giúp các bạn rèn luyện tư duy phản biện, nâng cao kỹ năng nghiên cứu, mà còn mở mang sự hiểu biết về pháp luật của mình. Ngoài ra, về khía cạnh quan hệ, mooting cũng tạo cơ hội để bạn gặp gỡ, giao lưu với những người giỏi trong ngành, những người có cùng đam mê, sở thích, hay thậm chí những người “đồng nghiệp tương lai” của các bạn sau này.</p>
+                    <p>Trong bất kỳ chuyện chinh phục nào, sự yêu và hiểu đối phương luôn là điều tối quan trọng. Vậy nên, tại MSS, mình, với tư cách là Nhà sáng lập và Người hướng dẫn, hy vọng có thể khiến bạn “yêu” và “hiểu” mooting hơn qua những bài giảng, những lời khuyên và những câu chuyện được chia sẻ. Và rồi, kể cả khi chinh phục được cuộc thi moot nào đó hay không, các bạn đều có thể trở thành mình – người truyền cảm hứng, dẫn dắt cho các thế hệ mooter tiếp theo. Từ đó, tất cả chúng ta sẽ góp phần nâng cao chất lượng và trình độ chung của cộng đồng mooting tại Việt Nam.</p>
+                    <p>Cảm ơn các bạn đã cho mình và MSS cơ hội được đồng hành cùng với các bạn trong chặng đường mooting. Mình hy vọng sẽ được gặp các bạn tại MSS.</p>
+                </div>
+                <div class="founder-words eng">
+                    <p>Welcome to Mooting Summer School (MSS). </p>
+                    <p>I believe that mooting is an activity every law student should experience at least once in their law school life. From a professional perspective, this is a challenging playground that not only helps you practice critical thinking, improve your research skills, but also expand your understanding of the law. Additionally, mooting is an opportunity for you to meet and interact with incredible people in the industry, people with the same passions and hobbies, or even people who might be your “future colleagues.”</p>
+                    <p>Loving and understanding your subject is always an important factor in any conquest. Therefore, at MSS, as the founder and instructor at MSS, I aim to help you “love” and “understand” mooting more through lectures, tips, and shared stories. And then, even if you conquer a moot competition or not, you can still become an inspiration and guide for the next generation of mooters. This way, we can all contribute to improving the quality and overall level of the mooting community in Vietnam.</p>
+                    <p>Thank you for allowing MSS and me to accompany you on your mooting journey. I hope to see you at MSS.</p>
+                </div>
+                <h5 class="text-end mt-5 vn" style="margin-top: -1rem">Đọc thêm thông tin về Nhà sáng lập tại đây</h5>
+                <h5 class="text-end mt-5 eng" style="margin-top: -1rem">You can read more about the Founder here</h5>
+                <div class="founder-words-img position-absolute">
+                    <h3 style="font-size:4.2rem; line-height:0.5rem; " class="vn">Lời ngỏ từ</h3>
+                    <h3 style="font-size:4.2rem; line-height:0.5rem; " class="eng">Message from</h3>
+                    <h1 class="vn">Nhà sáng lập</h1>
+                    <h1 class="eng">Our Founder</h1>
+                </div>
             </div>
         </div>
     </section>
+    <section class="our-mission background-full">
+        <div class="container p-5" style="color: #e5e5e5">
+            <div class="our-mission-title text-end vn">
+                <h1>Sứ mệnh</h1>
+                <h2 style="font-size:7rem; line-height:5rem;" >của MSS</h2>
+            </div>
+            <h1 class="eng text-center">Our mission</h1>
+            <div class="col-lg-8 vn" style="margin-top: -7rem">
+                <p>Trong những năm gần đây, mooting có xu hướng phát triển và phổ biến hơn tại khắp các trường đại học đào tạo về luật ở Việt Nam. Tuy nhiên, sự phát triển trong cộng đồng dường như chưa có tính tổng quát, hệ thống, và kế thừa. Các Mooter thường không được huấn một cách chuyên nghiệp và đầy đủ, do đó gặp khá nhiều khó khăn trong hành trình chinh chiến tại các giải đấu moot từ trong nước đến nước ngoài.</p>
+                <p>Trước bối cảnh đó, MSS ra đời với hy vọng có thể đặt những viên gạch nền tảng cho sự phát triển bền vững của cộng đồng. Là dự án đào tạo chuyên nghiệp về mooting đầu tiên tại Việt Nam, MSS kỳ vọng có thể đem lại cho học viên ba giá trị như sau:</p>
+                <ol style="list-style-type: lower-roman">
+                    <li>Nắm vững kỹ năng thiết yếu để đạt hiệu quả cao khi tham gia các cuộc thi moot;</li>
+                    <li>Xây dựng tư duy và thái độ phù hợp khi tham gia đấu moot; và</li>
+                    <li>Lan tỏa và phát triển tính cộng đồng trong mooting.</li>
+                </ol>
+            </div>
+            <div class="d-flex align-items-center justify-content-center ">
+                <div class="col-lg-8 eng shadow" style="margin-top: 0;">
+                    <p>In recent years, mooting has gained popularity in law schools over the last five years. However, the moot community lacks generalization, systemization, and inheritance in its development. Mooters are not typically professionally trained or led, resulting in many challenges and obstacles when competing in moot competitions, domestically and internationally.</p>
+                    <p>Given this context, MSS was established with the hope of laying the foundational bricks for the sustainable development of the community. As the first professional training project in Vietnam, MSS aims to provide its learners with three core values, as follows:.</p>
+                    <ol style="list-style-type: lower-roman">
+                        <li>To master foundational skills to achieve high efficiency when participating in mooting competitions;</li>
+                        <li>To build appropriate mindset, spirit, and attitude when participating in mooting;</li>
+                        <li>To spread and develop a sense of community in mooting.</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mascot-moto d-flex w-100" style="padding-top:0">
+        <div class=" col-lg-5">
+            <img src="./assets/imgs/about-3.png" alt="mss-bg" class="w-100">
+        </div>
+        <div class="p-5 vn" style="margin-top: 8rem">
+            <h1 class="text-end">Biểu tượng</h1>
+            <p>Gryffin, hay còn gọi là “điểu sư” trong tiếng Việt, là một linh vật thần thoại có phần đầu, cánh, móng vuốt của đại bàng và phần thân, đuôi, chân sau của sư tử. Vì đại bàng được coi là “vua của các loài chim” và sư tử là “vua của các loài thú”, nên điểu sư đại diện cho sức mạnh và sự thông thái trong nhiều nền văn hóa cổ như Ba Tư, Hy Lạp, Ai Cập, Ấn Độ v.v. </p> 
+            <p>MSS chọn điểu sư làm biểu tượng cho dự án cũng bởi hai nửa của nó đều có mối gắn bó mật thiết với MSS. Với nửa đại bàng, nó gợi nhắc về một phần tên gọi đội đấu moot đầu tiên của Nhà sáng lập. Còn nửa sư tử lại tương trưng cho lòng dũng cảm và ý chí mạnh mẽ của các mooters nói chung trong hành trình tham gia thi đấu.</p>
+        </div>
+        <div class="p-5 eng" style="margin-top: 8rem">
+            <h1 class="text-end">Our Mascot</h1>
+            <p>Gryffin, also known as "điểu sư" in Vietnamese, is a mythological creature with the head, wings, and talons of an eagle and the body, tail, and hind legs of a lion. Because the eagle is considered the "king of birds" and the lion is the "king of beasts," the Gryffin symbolizes strength and wisdom in many ancient cultures such as Persia, Greece, Egypt, India, and so on.</p> 
+            <p>MSS chose the Gryffin as its emblem for the project because both halves of it are closely connected to MSS. With the eagle half, it evokes a part of the name of the founder's first moot team. Meanwhile, the lion half symbolizes the courage and bravery of mooters in general throughout their competition journey.</p>
+        </div>
+    </section>
+    <section class="position-relative">
+        <div class="position-absolute" style="top:30%; left:-180px">
+            <div class="circle">
+                <div class="text" >
+                    <p>• Non est ad astra mollis e terris via</p>
+                </div>
+            </div>
+        </div>
+        <div class="position-absolute" style="top:5%; right:-50px">
+            <div class="circle" style="top:10%; right:-180px">
+                <div class="text">
+                    <p>• Non est ad astra mollis e terris via</p>
+                </div>
+            </div>
+        </div>
+        <div class="container col-6" >
+            <h1 class="text-center mt-5 vn">Triết lý</h1>
+            <h1 class="text-center mt-5 eng">Philosophy</h1>
+            
+            <strong style="font-family: 'Times New Roman', Times, serif; font-size: 2rem"><em> Non est ad astra mollis e terris
+                via</em></strong>
+            <p class="text-end vn" style="font-family: 'Times New Roman', Times, serif;"><em> - Megara, Hercales Furens của Seneca, dòng 437</em></p>
+            <p class="text-end eng" style="font-family: 'Times New Roman', Times, serif;"><em> - Megara, Hercales Furens of Seneca, line 437</em></p>
+            <p class="vn"><em>“Non est ad astra mollis e terris via”</em>, tạm dịch: <em>“Chẳng có con đường nào dễ dàng để vươn tới những vì sao”</em>, dường như là phép ẩn dụ hoàn hảo khi nói về hành trình thi đấu moot của bất cứ ai. Bởi lẽ, mooting có thể trở thành “ngôi sao may mắn” và làm thay đổi tích cực tương lai của ai đó trong nghề luật. Nhưng để hái được ngôi sao như vậy, các mooter phải trải qua rất nhiều khó khăn, thậm chí là thất bại hết lần này đến lần khác. MSS chọn đây là triết lý của dự án, một là để nhắc nhở mình về sứ mệnh xây dựng một bước đệm nền tảng cho các học viên tiếp tục hành trình chinh phục mooting. Đồng thời, thông qua triết lý này, MSS cũng muốn nhắn nhủ đến các học viên rằng chặng đường với mooting còn dài và khó khăn, vì thế đừng chủ quan nhưng cũng đừng nản lòng. Sau tất cả, MSS tin rằng bạn sẽ luôn nhận được giá trị xứng đáng với những gì đã bỏ ra.</p>
+            <p class="eng"><em>“Non est ad astra mollis e terris via”</em>, roughly translated as <em>"There is no easy path from the earth to the stars"</em>, seems to be a fitting metaphor for the challenges one faces in mooting. Because indeed, mooting can become a "lucky star" and positively change someone's future in the legal profession. However, to harvest such a star, mooters must endure many difficulties, even failures time and time again. MSS chooses this as the project's philosophy, firstly to remind itself of the mission to lay a foundational stepping stone for students to continue their journey in conquering mooting. Moreover, through this philosophy, MSS also wants to convey to students that the path with mooting is arduous and lengthy, so they should remain determined, yet not be discouraged. After all, MSS believes that you will always gain the deserved value from this journey for the effort you’ve put in.</p>
+        </div>
+    </section>
 
-    <section class="" id="services">
+    <!-- About the MSS team -->
+    <!-- <section class="" id="services">
         <div class="container">
             <div class="text-center">
                 <h1 class="section-heading text-uppercase">Our Team</h1>
@@ -21,7 +109,6 @@
                     <span class="fa-stack fa-4x">
                         <a href="#">
                             <i class="fas fa-circle fa-stack-2x main-text-color"></i>
-                            <!-- <i class="fas fa-solid fa-eye fa-stack-1x fa-inverse"></i> -->
                         </a>
                     </span>
                     <h4 class="my-3">Name</h4>
@@ -31,7 +118,7 @@
                     <span class="fa-stack fa-4x">
                         <a href="#">
                             <i class="fas fa-circle fa-stack-2x main-text-color"></i>
-                            <!-- <i class="fas fa-solid fa-mortar-pestle fa-stack-1x fa-inverse"></i> -->
+                           
                         </a>
                     </span>
                     <h4 class="my-3">Name</h4>
@@ -42,7 +129,7 @@
                     <span class="fa-stack fa-4x">
                         <a href="#">
                             <i class="fas fa-circle fa-stack-2x main-text-color"></i>
-                            <!-- <i class="fas far fa-grin-hearts fa-stack-1x fa-inverse"></i> -->
+                            
                         </a>
                     </span>
                     <h4 class="my-3">Name</h4>
@@ -51,5 +138,5 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
 </main>
