b86f86bc98848aa60dde982a15d71955c91bfb53
Thi Lam <z.thiilam@gmail.com>
2024-05-04 13:56:38 +0700

    styling About Page

--- views/about.php
+++ views/about.php
@@ -1,5 +1,5 @@
-<main id="ab-main">
-    <section class="container">
+<main id="ab-main" class="w-100">
+    <section class="container" style="padding-top:0">
         <div class="d-lg-flex">
             <img src="./assets/imgs/about-1.png" alt="founder-Vo-Son-Hoang" style="height: 100%;">
             <div class="px-5 position-relative" style="font-size: 1.1rem;">
@@ -15,8 +15,8 @@
                     <p>Loving and understanding your subject is always an important factor in any conquest. Therefore, at MSS, as the founder and instructor at MSS, I aim to help you “love” and “understand” mooting more through lectures, tips, and shared stories. And then, even if you conquer a moot competition or not, you can still become an inspiration and guide for the next generation of mooters. This way, we can all contribute to improving the quality and overall level of the mooting community in Vietnam.</p>
                     <p>Thank you for allowing MSS and me to accompany you on your mooting journey. I hope to see you at MSS.</p>
                 </div>
-                <h5 class="text-end mt-5 vn">Đọc thêm thông tin về Nhà sáng lập tại đây</h5>
-                <h5 class="text-end mt-5 eng">You can read more about the Founder here</h5>
+                <h5 class="text-end mt-5 vn" style="margin-top: -1rem">Đọc thêm thông tin về Nhà sáng lập tại đây</h5>
+                <h5 class="text-end mt-5 eng" style="margin-top: -1rem">You can read more about the Founder here</h5>
                 <div class="founder-words-img position-absolute">
                     <h3 style="font-size:4.2rem; line-height:0.5rem; " class="vn">Lời ngỏ từ</h3>
                     <h3 style="font-size:4.2rem; line-height:0.5rem; " class="eng">Message from</h3>
@@ -32,7 +32,7 @@
                 <h1>Sứ mệnh</h1>
                 <h2 style="font-size:7rem; line-height:5rem;" >của MSS</h2>
             </div>
-            <h1 class="eng">Our mission</h1>
+            <h1 class="eng text-center">Our mission</h1>
             <div class="col-lg-8 vn" style="margin-top: -7rem">
                 <p>Trong những năm gần đây, mooting có xu hướng phát triển và phổ biến hơn tại khắp các trường đại học đào tạo về luật ở Việt Nam. Tuy nhiên, sự phát triển trong cộng đồng dường như chưa có tính tổng quát, hệ thống, và kế thừa. Các Mooter thường không được huấn một cách chuyên nghiệp và đầy đủ, do đó gặp khá nhiều khó khăn trong hành trình chinh chiến tại các giải đấu moot từ trong nước đến nước ngoài.</p>
                 <p>Trước bối cảnh đó, MSS ra đời với hy vọng có thể đặt những viên gạch nền tảng cho sự phát triển bền vững của cộng đồng. Là dự án đào tạo chuyên nghiệp về mooting đầu tiên tại Việt Nam, MSS kỳ vọng có thể đem lại cho học viên ba giá trị như sau:</p>
@@ -42,8 +42,8 @@
                     <li>Lan tỏa và phát triển tính cộng đồng trong mooting.</li>
                 </ol>
             </div>
-            <div class="d-flex align-items-center justify-content-center eng">
-                <div class="col-lg-8">
+            <div class="d-flex align-items-center justify-content-center ">
+                <div class="col-lg-8 eng shadow" style="margin-top: 0;">
                     <p>In recent years, mooting has gained popularity in law schools over the last five years. However, the moot community lacks generalization, systemization, and inheritance in its development. Mooters are not typically professionally trained or led, resulting in many challenges and obstacles when competing in moot competitions, domestically and internationally.</p>
                     <p>Given this context, MSS was established with the hope of laying the foundational bricks for the sustainable development of the community. As the first professional training project in Vietnam, MSS aims to provide its learners with three core values, as follows:.</p>
                     <ol style="list-style-type: lower-roman">
@@ -55,17 +55,17 @@
             </div>
         </div>
     </section>
-    <section class="mascot-moto d-flex">
+    <section class="mascot-moto d-flex w-100" style="padding-top:0">
         <div class=" col-lg-5">
             <img src="./assets/imgs/about-3.png" alt="mss-bg" class="w-100">
         </div>
-        <div class="p-5 vn">
-            <h1 class="text-end">Biểu tượng Griffin</h1>
+        <div class="p-5 vn" style="margin-top: 8rem">
+            <h1 class="text-end">Biểu tượng</h1>
             <p>Gryffin, hay còn gọi là “điểu sư” trong tiếng Việt, là một linh vật thần thoại có phần đầu, cánh, móng vuốt của đại bàng và phần thân, đuôi, chân sau của sư tử. Vì đại bàng được coi là “vua của các loài chim” và sư tử là “vua của các loài thú”, nên điểu sư đại diện cho sức mạnh và sự thông thái trong nhiều nền văn hóa cổ như Ba Tư, Hy Lạp, Ai Cập, Ấn Độ v.v. </p> 
             <p>MSS chọn điểu sư làm biểu tượng cho dự án cũng bởi hai nửa của nó đều có mối gắn bó mật thiết với MSS. Với nửa đại bàng, nó gợi nhắc về một phần tên gọi đội đấu moot đầu tiên của Nhà sáng lập. Còn nửa sư tử lại tương trưng cho lòng dũng cảm và ý chí mạnh mẽ của các mooters nói chung trong hành trình tham gia thi đấu.</p>
         </div>
-        <div class="p-5 eng">
-            <h1 class="text-end">Meet Our Mascot</h1>
+        <div class="p-5 eng" style="margin-top: 8rem">
+            <h1 class="text-end">Our Mascot</h1>
             <p>Gryffin, also known as "điểu sư" in Vietnamese, is a mythological creature with the head, wings, and talons of an eagle and the body, tail, and hind legs of a lion. Because the eagle is considered the "king of birds" and the lion is the "king of beasts," the Gryffin symbolizes strength and wisdom in many ancient cultures such as Persia, Greece, Egypt, India, and so on.</p> 
             <p>MSS chose the Gryffin as its emblem for the project because both halves of it are closely connected to MSS. With the eagle half, it evokes a part of the name of the founder's first moot team. Meanwhile, the lion half symbolizes the courage and bravery of mooters in general throughout their competition journey.</p>
         </div>
