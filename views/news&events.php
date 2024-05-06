5779e31e5abc38fe7b45de604338540067837385
DKVI <92564346+DKVI@users.noreply.github.com>
2024-05-06 18:14:02 +0700

    done news and events page

--- views/news&events.php
+++ views/news&events.php
@@ -1,5 +1,4 @@
 <?php
-$popularList = Post::getPopularPost($conn, 3);
 $newsList = Post::getByType($conn, 'news', 2);
 $eventList = Post::getByType($conn, 'event', 2);
 $mainPost = Post::getById($conn, '48864jf325949f3553');
@@ -15,17 +14,20 @@ $mainPost = Post::getById($conn, '48864jf325949f3553');
 
     <!-- Main content -->
     <div class="container py-5 p-lg-5 w-100">
-        <div class="row">
-            <div class="col-lg-8 h-100 row">
+        <div class="row justify-content-center min-vh-100 " style="gap: 16px">
+            <div class="col-lg-8 h-100 row shadow-lg rounded-3">
                 <div class="col-lg-12">
                     <img src="./uploads/imgs/default.png">
                 </div>
                 <div class="col-lg-6 p-4"><a>
-                        <h1>ABOUT M.S.S</h1>
+                        <h1 style="font-size: 50px">ABOUT M.S.S</h1>
                     </a></div>
                 <div class="col-lg-6 p-4">
                     <div>
-                        <p>Trong bất kỳ chuyện chinh phục nào, sự yêu và hiểu đối phương luôn là điều tối quan trọng.
+                        <p class="" style="text-align: left">Trong bất kỳ chuyện chinh phục nào, sự yêu và hiểu đối
+                            phương
+                            luôn là điều
+                            tối quan trọng.
                             Vậy nên, tại MSS, mình, với tư cách là Nhà sáng lập và Người hướng dẫn, hy vọng có thể khiến
                             bạn “yêu” và “hiểu” mooting hơn qua những bài giảng, những lời khuyên và những câu chuyện
                             được chia sẻ. </p>
@@ -33,7 +35,7 @@ $mainPost = Post::getById($conn, '48864jf325949f3553');
                     <div><a class="btn" style="background-color: #274069; color: white" href="">READ MORE</a></div>
                 </div>
             </div>
-            <div class="col-lg-4 h-100 p-3 d-flex flex-column" style="background-color: #274069">
+            <div class="col-lg-4 h-100 p-3 d-flex flex-column rounded-3 shadow-lg" style="background-color: #274069">
                 <h2 style="color: white; height: 10%">
                     NEWS & EVENTS
                 </h2>
@@ -41,11 +43,11 @@ $mainPost = Post::getById($conn, '48864jf325949f3553');
                     <?php
                     foreach ($newsList as $news) {
                         echo '
-                        <div class="w-100 h-25 py-4" style="border-top: 1px solid white">
+                        <a href="' . BASE_URL . $news->path . '" class="w-100 h-25 py-4 px-3" style="border-top: 1px solid white; color: white">
                             <h4 style="overflow: hidden;
                                             text-overflow: ellipsis;
                                             white-space: nowrap;">' . $news->title . '</h4>
-                            <p class="" style="display: -webkit-box;
+                            <p  style="display: -webkit-box; 
                                 -webkit-line-clamp: 2;
                                 -webkit-box-orient: vertical;
                                 overflow: hidden;
@@ -54,11 +56,11 @@ $mainPost = Post::getById($conn, '48864jf325949f3553');
                     </p>
                     <p
                         style="font-size: 14px">' . convertDate($news->date) . " - " . $news->readTimes . " minutes read" . '</p>
-                </div>';
+                </a>';
                     }
                     foreach ($eventList as $event) {
                         echo '
-                        <div class="w-100 h-25 py-4" style="border-top: 1px solid white">
+                        <a href="' . BASE_URL . $event->path . '" class="w-100 h-25 py-4 px-3" style="border-top: 1px solid white; color: white">
                             <h4 style="overflow: hidden;
                                             text-overflow: ellipsis;
                                             white-space: nowrap;">' . $event->title . '</h4>
@@ -66,18 +68,21 @@ $mainPost = Post::getById($conn, '48864jf325949f3553');
                                 -webkit-line-clamp: 2;
                                 -webkit-box-orient: vertical;
                                 overflow: hidden;
-                                text-overflow: ellipsis; font-size: 16px">
+                                text-overflow: ellipsis; font-size: 16px; text-align: left">
                                 ' . clearSpecialSymbols($event->content) . '
                     </p>
                     <p
                         style="font-size: 14px">' . convertDate($event->date) . " - " . $event->readTimes . " minutes read" . '</p>
-                </div>';
+                </a>';
                     }
                     ?>
                 </div>
             </div>
         </div>
-        <div class="row"></div>
+        <div class="row mt-3">
+
+            <?php include "./views/components/popularPost.php" ?>
+        </div>
     </div>
 
 </main>
