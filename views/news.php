635e9ca8cda6672edaab5fa95b3f5f1445a1b44c
DKVI <CT06N0164@actvn.edu.vn>
2024-06-08 17:59:24 +0700

    add date and reading time to post page

--- views/news.php
+++ views/news.php
@@ -15,7 +15,9 @@ $relativePosts = Post::getRelatedPost($conn, $id, 3);
 
 ?>
 <script src="<?php echo BASE_URL . "/vendor/marked.js" ?>" defer></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+    integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <main class="position-relative" style="margin-top: 10rem">
     <div class="position-fixed d-flex justify-content-end" style="left: 0;
@@ -72,52 +74,54 @@ $relativePosts = Post::getRelatedPost($conn, $id, 3);
 
 
 <script defer>
-    async function convertMarkdown() {
-        const response = await fetch("<?php echo BASE_URL . "/uploads/posts/" . $post->id . ".md" ?>");
-        const markdown = await response.text();
-        const html = marked.parse(markdown);
-        console.log(html);
-        document.getElementById("markdown-content").innerHTML = "<h1><?php echo $post->title ?></h1>" + html;
-        // const iframeList = document.querySelectorAll("iframe");
-        // iframeList.forEach(element => {
-        //     const item =
-        // });
+async function convertMarkdown() {
+    const response = await fetch("<?php echo BASE_URL . "/uploads/posts/" . $post->id . ".md" ?>");
+    const markdown = await response.text();
+    const html = marked.parse(markdown);
+    console.log(html);
+    document.getElementById("markdown-content").innerHTML =
+        "<h1><?php echo $post->title ?></h1><div class='fst-italic' style='text-align: left;'><span><?php echo convertDate($post->date) ?></span> | <span><?php echo 'Reading time: ' . $post->readTimes . " minutes" ?></span></div><br></br>" +
+        html;
+    // const iframeList = document.querySelectorAll("iframe");
+    // iframeList.forEach(element => {
+    //     const item =
+    // });
 
-    }
-    convertMarkdown();
+}
+convertMarkdown();
 </script>
 
 
 <style>
-    #markdown-content {
-        padding: 32px;
-        border-radius: 16px;
-    }
+#markdown-content {
+    padding: 32px;
+    border-radius: 16px;
+}
 
-    #markdown-content h1 {
-        font-size: 3.5rem;
-        padding-bottom: 2rem;
-        text-align: left;
-    }
+#markdown-content h1 {
+    font-size: 3.5rem;
+    padding-bottom: 2rem;
+    text-align: left;
+}
 
-    #markdown-content ifram {
-        width: 100%;
-    }
+#markdown-content ifram {
+    width: 100%;
+}
 
-    #markdown-content p {
-        font-size: 1.2rem;
-        display: flex;
-        margin-bottom: 1.5rem;
-        text-align: left;
-    }
+#markdown-content p {
+    font-size: 1.2rem;
+    display: flex;
+    margin-bottom: 1.5rem;
+    text-align: left;
+}
 
-    #markdown-content p img {
-        margin: auto;
-        width: 75%;
-    }
+#markdown-content p img {
+    margin: auto;
+    width: 75%;
+}
 
-    #markdown-content p em {
-        margin: auto;
-        font-size: 14px;
-    }
+#markdown-content p em {
+    margin: auto;
+    font-size: 14px;
+}
 </style>
